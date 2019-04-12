b211287f8ddc53a2d813d65c1c664b6beb2f02e5
lehoangvuvt <hoangvule100@gmail.com>
2019-04-12 16:15:41 +0700

    Updates

---
+++ precache-manifest.f3e5a56fef0969c0f7adf553f2441739.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "95abffdf2b9b43c13fae",
+    "url": "/memecreator/static/js/runtime~main.a26b4ca0.js"
+  },
+  {
+    "revision": "b19350fc112659bd2215",
+    "url": "/memecreator/static/js/main.9654fc69.chunk.js"
+  },
+  {
+    "revision": "0f19e2518a3a17b37f23",
+    "url": "/memecreator/static/js/2.e2adb5f9.chunk.js"
+  },
+  {
+    "revision": "b19350fc112659bd2215",
+    "url": "/memecreator/static/css/main.79be670f.chunk.css"
+  },
+  {
+    "revision": "8fe93abfa0ab2495dc7581618789c479",
+    "url": "/memecreator/index.html"
+  }
+];
